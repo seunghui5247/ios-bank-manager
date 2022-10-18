02ab343181ad7ef492d139ac9002834b5c394665
seunghui5247 <seunghui5247@gmail.com>
2022-10-18 15:28:48 +0900

    refactor: LinkedList 타입 안에서만 쓰는 변수 은닉화

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct LinkedList<T> {
-    var head: Node<T>?
-    var tail: Node<T>?
+    private var head: Node<T>?
+    private var tail: Node<T>?
     
     var isEmpty: Bool {
         head == nil
